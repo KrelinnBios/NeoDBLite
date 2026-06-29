94a3b0288c4181fc31043c710a4d8c2fbb345679
Bios <krelinn@outlook.com>
2026-06-30 03:13:11 +0800

    创建 ProfileHtmlParserTest.kt

---
+++ app/src/test/java/com/krelinnbios/neodblite/util/ProfileHtmlParserTest.kt
@@ -0,0 +1,30 @@
+﻿package com.krelinnbios.neodblite.util
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ProfileHtmlParserTest {
+    @Test
+    fun parsesParagraphBioFromProfileDiv() {
+        val html = """
+            <main>
+              <div class="layout"><p>Ignore this short block</p></div>
+              <div>
+                <p>Profile intro:</p><p>Line one<br>Line two<br>Line three</p><p>Final note</p>
+                <br>
+              </div>
+            </main>
+        """.trimIndent()
+
+        assertEquals(
+            """
+            Profile intro:
+            Line one
+            Line two
+            Line three
+            Final note
+            """.trimIndent(),
+            ProfileHtmlParser.parseBio(html)
+        )
+    }
+}
